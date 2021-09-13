893b8a92d74ec2f92b891e0b8933d2dbd07989f2
josh <41476440+Joshua-Hwang@users.noreply.github.com>
2021-09-13 22:27:13 +1000

    sphere normals

--- 04/main.cpp
+++ 04/main.cpp
@@ -10,27 +10,42 @@
 
 #define FILE_NAME "out.png"
 
-bool hit_sphere(const Point3 &center, double radius, const Ray &r) {
+/**
+* If hitpoint exists
+* will return the distance to the sphere
+* else
+* will return -1
+*/
+double hit_sphere(const Point3 &center, double radius, const Ray &r) {
   // from origin to center
   Vec3 oc = r.origin() - center;
-  auto a = dot(r.direction(), r.direction());
-  auto b = 2.0 * dot(oc, r.direction());
-  auto c = dot(oc, oc) - radius*radius;
-  auto discriminant = b * b - 4 * a * c;
+  auto a = r.direction().length_squared();
+  auto half_b = dot(oc, r.direction());
+  auto c = oc.length_squared() - radius * radius;
+  auto discriminant = half_b * half_b - a * c;
   // the discriminant is all that is needed. If a solution exists
   // (discriminant == 0) then we have a single solution, the ray grazes the sphere
   // (discriminant > 0) then we two solution and the ray entered the sphere
-  return (discriminant > 0);
+  if (discriminant < 0) {
+    return -1.0;
+  }
+  // some simplification of the quadratic formula
+  return (-half_b - sqrt(discriminant)) / a;
 }
 
 Color ray_color(const Ray &r) {
+  auto t = hit_sphere(Point3(0, 0, -1), 0.5, r);
   // sphere location placed at z = -1 right in front of the camera
-  if (hit_sphere(Point3(0, 0, -1), 0.5, r)) {
-    return Color(1, 0, 0);
+  if (t > 0.0) {
+    // normal vector
+    Vec3 N = unit_vector(r.at(t) - Vec3(0, 0, -1));
+    // to avoid negative colors
+    return 0.5 * Color(N.x() + 1, N.y() + 1, N.z() + 1);
   }
+  // Didn't hit anything so this is empty air
   Vec3 unit_direction = unit_vector(r.direction());
   // t is arbitrarily set as a single step of the y direction
-  auto t = 0.5 * (unit_direction.y() + 1.0);
+  t = 0.5 * (unit_direction.y() + 1.0);
   // As time continues the ray loses color.
   return (t) * Color(1.0, 1.0, 1.0) + (1.0 - t) * Color(0.5, 0.7, 1.0);
 }
