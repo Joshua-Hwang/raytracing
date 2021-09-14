b132d68e620b67063b7f8e87971c69286edb7d60
josh <41476440+Joshua-Hwang@users.noreply.github.com>
2021-09-14 19:35:28 +1000

    completed chapter six

--- 04/main.cpp
+++ 04/main.cpp
@@ -1,53 +1,26 @@
-#include "vec3.h"
+#include "rtweekend.h"
 #include "color.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 #include "../external/lodepng/lodepng.h"
 
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
-#include <vector>
+
+using std::make_shared;
 
 #define FILE_NAME "out.png"
 
-/**
-* If hitpoint exists
-* will return the distance to the sphere
-* else
-* will return -1
-*/
-double hit_sphere(const Point3 &center, double radius, const Ray &r) {
-  // from origin to center
-  Vec3 oc = r.origin() - center;
-  auto a = r.direction().length_squared();
-  auto half_b = dot(oc, r.direction());
-  auto c = oc.length_squared() - radius * radius;
-  auto discriminant = half_b * half_b - a * c;
-  // the discriminant is all that is needed. If a solution exists
-  // (discriminant == 0) then we have a single solution, the ray grazes the sphere
-  // (discriminant > 0) then we two solution and the ray entered the sphere
-  if (discriminant < 0) {
-    return -1.0;
-  }
-  // some simplification of the quadratic formula
-  return (-half_b - sqrt(discriminant)) / a;
-}
-
-Color ray_color(const Ray &r) {
-  auto t = hit_sphere(Point3(0, 0, -1), 0.5, r);
-  // sphere location placed at z = -1 right in front of the camera
-  if (t > 0.0) {
-    // normal vector
-    Vec3 N = unit_vector(r.at(t) - Vec3(0, 0, -1));
-    // to avoid negative colors
-    return 0.5 * Color(N.x() + 1, N.y() + 1, N.z() + 1);
+Color ray_color(const Ray &r, const Hittable &world) {
+  HitRecord rec;
+  if (world.hit(r, 0, infinity, rec)) {
+    return 0.5 * (rec.normal + Color(1,1,1));
   }
   // Didn't hit anything so this is empty air
   Vec3 unit_direction = unit_vector(r.direction());
   // t is arbitrarily set as a single step of the y direction
-  t = 0.5 * (unit_direction.y() + 1.0);
+  auto t = 0.5 * (unit_direction.y() + 1.0);
   // As time continues the ray loses color.
-  return (t) * Color(1.0, 1.0, 1.0) + (1.0 - t) * Color(0.5, 0.7, 1.0);
+  return (1.0 - t) * Color(1.0, 1.0, 1.0) + (t) * Color(0.5, 0.7, 1.0);
 }
 
 int main(int argc, char *argv[]) {
@@ -55,6 +28,11 @@ int main(int argc, char *argv[]) {
   const auto aspect_ratio = 16.0 / 9.0;
   const unsigned image_width = 400;
   const unsigned image_height = static_cast<int>(image_width / aspect_ratio);
+
+  // World
+  HittableList world;
+  world.add(make_shared<Sphere>(Point3(0,0,-1), 0.5));
+  world.add(make_shared<Sphere>(Point3(0,-100.5,-1), 100));
 
   // Camera
   auto viewport_height = 2.0;
@@ -68,7 +46,7 @@ int main(int argc, char *argv[]) {
   // z component if we want to rays to go anywhere
   // as the name suggests it points to the most bottom left ray. The for loop builds
   // the other rays from this point for implementation simplicity
-  auto upper_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
+  auto lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
 
   // Render
   Image img(image_width, image_height);
@@ -78,9 +56,9 @@ int main(int argc, char *argv[]) {
     for (int x = 0; x < image_width; ++x) {
       auto u = double(x) / (image_width - 1);
       auto v = double(y) / (image_height - 1);
-      Ray r(origin, upper_left_corner + u * horizontal + v * vertical - origin);
-      Color pixel_color = ray_color(r);
-      img.set({x,y}, pixel_color);
+      Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+      Color pixel_color = ray_color(r, world);
+      img.set({x,image_height - y}, pixel_color);
     }
   }
   std::cerr << "\nSaving file...\n";
