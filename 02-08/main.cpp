3bc8032df6ccbe05f5c7fb7894aa813e57559f93
Joshua <41476440+Joshua-Hwang@users.noreply.github.com>
2021-11-14 18:12:35 +1000

    chap 8 done

--- 02-08/main.cpp
+++ 02-08/main.cpp
@@ -172,8 +172,15 @@ HittableList cornell_box() {
   objects.add(std::make_shared<xyRect>(0, 555, 0, 555, 555, white));
 
   // boxes
-  objects.add(std::make_shared<Box>(Point3(130, 0, 65), Point3(295, 165, 230), white));
-  objects.add(std::make_shared<Box>(Point3(265, 0, 295), Point3(430, 330, 460), white));
+  std::shared_ptr<Hittable> box1 = std::make_shared<Box>(Point3(0, 0, 0), Point3(165, 330, 165), white);
+  box1 = std::make_shared<rotateY>(box1, 15);
+  box1 = std::make_shared<Translate>(box1, Vec3(265, 0, 295));  
+  objects.add(box1);
+
+  std::shared_ptr<Hittable> box2 = std::make_shared<Box>(Point3(0, 0, 0), Point3(165, 165, 165), white);
+  box2 = std::make_shared<rotateY>(box2, -18);
+  box2 = std::make_shared<Translate>(box2, Vec3(130,0,65));
+  objects.add(box2);
 
   return objects;
 }
